7534e9c06fea3dec6b6f180aedf7fa2c73c73fe3
Jeevakathir005 <Jeevakathir005@gmail.com>
2022-02-11 20:36:43 +0530

    commit

---
+++ 3_implementation/func.h
@@ -0,0 +1,8 @@
+#ifndef FUNC_H_INCLUDED
+#define FUNC_H_INCLUDED
+void avalaibility();
+void bill();
+int menu();
+int d=20,r=35,p=35,v=5,n=50,sv=15,tv=15,vfr=50,nfr=75,i=8;
+    int d1,r1,p1,v1,n1,n1,sv1,tv1,vfr1,nfr1,i1;
+#endif
